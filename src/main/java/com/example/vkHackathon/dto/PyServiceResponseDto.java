ca9d44cfa2f825e325e25fe8247741bc7af9942a
Assem <assemay_maratbek@epam.com>
2025-06-13 18:48:43 +0500

    added PyServiceResponseDto

--- src/main/java/com/example/vkHackathon/dto/PyServiceResponseDto.java
+++ src/main/java/com/example/vkHackathon/dto/PyServiceResponseDto.java
@@ -1,7 +1,33 @@
 package com.example.vkHackathon.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Data
 public class PyServiceResponseDto {
+    private String contractNumber;
+    @JsonFormat(pattern = "dd.MM.yyyy")
+    private LocalDate contractDate;
+    private String contractSubject;
+    private BigDecimal contractAmount;
+    private String contractCurrency;
+    private String paymentCurrency;
+    private int executionPeriod;
+    private int paymentTerms;
+    private String tnvedCode;
+    private boolean liabilityClauses;
+    private int repatriationPeriod;
+    private String contractKNP;
+    private String contractKVO;
+    private boolean relatedToRF_RB_UA;
+    private String foreignPartnerName;
+    private String foreignPartnerBin;
+    private String foreignPartnerCountry;
+    private String foreignPartnerAccount;
+    private String foreignPartnerBankSwift;
+    private String foreignPartnerAddress;
+    private boolean hasThirdParties;
 }
